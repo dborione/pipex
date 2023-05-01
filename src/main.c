09db6f327444b14e1b481df89b42a098eb168721
Donatienne Borione <db1997@outlook.fr>
2023-05-01 13:30:24 +0200

    started working on pipes

--- src/main.c
+++ src/main.c
@@ -1,43 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dborione <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/01 12:45:02 by dborione          #+#    #+#             */
+/*   Updated: 2023/05/01 13:29:58 by dborione         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
 
 //int execve(const char *pathname, char *const argv[], char *const envp[]);
+//fd = open(argv[1], O_RDWR);
+//write(fd, "1", 1);
 
 int	main(int argc, char **argv) {
+	int pipe_fd[2]; //nbr of cmds]
+	pipe(pipe_fd);
+	// fd[0] - read
+	// fd[1] - write
 	int	fd;
 	int process_id;
 	
 	char *file = "/bin/echo";
-	char *args[] = {"echo", "$first", NULL};
-	char *env[] = {"first=fdsfds", NULL};
+	char *args2 = strcat(argv[1], " child process");
+	char *args[] = {"/bin/echo", args2, NULL};
+	char *env[] = {NULL};
 
 	process_id = fork();
 	if (process_id == -1)
 		return (1);
 
 	if (process_id == 0)
-		execve(file, args, env);
-
-		//recupere l'output
-	else {
+	{
+		//close(fd[0]);
+		//write to the pipe = write(fd[1])
+		int id2 = fork();
+		if (id2 == 0)
+			execve(file, args, env);
+		wait(NULL);
+		char *buf = malloc(5556564545645);
+		char *result = read(1, buf, sizeof(buf));
+		write(fd[1], result, sizeof(result));
+	} else {
 		wait(NULL);// -> wait for child processes to finish
 		//execve cmd2 avec l'output du child
+		//read from the pipe = read(fd[0])
 		printf("parent process");
 	}
 
 
 
 
-	//fd = open(argv[1], O_RDWR);
-	//write(fd, "1", 1);
+
 	
 
 //	char *file = "/usr/bin/whereis";
-//	char *args[] = {"/usr/bin/whereis", argv[1], NULL};
-//	char *env[] = {NULL};
-
+//	char *args[] = {"/usr/bin/whereis", $ENV, NULL};
+//	char *env[] = {"ENV=fjdjs", NULL};
 
 	return (0);
 }
