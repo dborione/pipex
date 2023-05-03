50fadf35a20a21918782aa808bfd03487c56966b
Donatienne Borione <db1997@outlook.fr>
2023-05-03 12:45:24 +0200

    add stdout

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dborione <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 12:45:02 by dborione          #+#    #+#             */
-/*   Updated: 2023/05/01 13:29:58 by dborione         ###   ########.fr       */
+/*   Updated: 2023/05/03 12:44:34 by dborione         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,17 @@
 
 //int execve(const char *pathname, char *const argv[], char *const envp[]);
 //fd = open(argv[1], O_RDWR);
-//write(fd, "1", 1);
+//write(fd, "1", 1)
+// fd[0] - read
+// fd[1] - write
 
 int	main(int argc, char **argv) {
 	int pipe_fd[2]; //nbr of cmds]
-	pipe(pipe_fd);
-	// fd[0] - read
-	// fd[1] - write
+	if (pipe(pipe_fd) == -1)
+	{
+		perror("Error");
+		return (1);
+	}
 	int	fd;
 	int process_id;
 	
@@ -38,18 +42,18 @@ int	main(int argc, char **argv) {
 	if (process_id == -1)
 		return (1);
 
-	if (process_id == 0)
+	if (process_id == 0) // Child Process
 	{
 		//close(fd[0]);
 		//write to the pipe = write(fd[1])
 		int id2 = fork();
 		if (id2 == 0)
 			execve(file, args, env);
-		wait(NULL);
 		char *buf = malloc(5556564545645);
-		char *result = read(1, buf, sizeof(buf));
-		write(fd[1], result, sizeof(result));
-	} else {
+		char *result = read(STDOUT_FILENO, buf, sizeof(buf));
+		//write(fd[1], result, sizeof(result));
+	
+	} else { // Parent Process
 		wait(NULL);// -> wait for child processes to finish
 		//execve cmd2 avec l'output du child
 		//read from the pipe = read(fd[0])
