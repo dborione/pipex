96aa2a92c804a24ee65778c9a51b40efe01efd51
Donatienne Borione <db1997@outlook.fr>
2023-05-26 15:37:01 +0200

    dup2 not working

--- src/forks.c
+++ src/forks.c
@@ -1,20 +1,20 @@
 #include "../includes/pipex.h"
 
-void	exec(t_cmd *cmd, char **env)
+void	exec(char **argv, t_cmd *cmd, char **env)
 {
-	char *arg[] = {cmd->cmd_path, cmd->cmd_param, cmd->file, cmd->ptr};
+	char *arg[] = {cmd->cmd_path, cmd->cmd_param, NULL};
 	if (execve(cmd->cmd_path, arg, env) == -1)
-        ft_error(0, "cmd execution error");
+        ft_error(0, "cmd exec1");
 }
 
 void	ft_open_files(char **argv, int argc, t_pipex *pipex)
 {
 	pipex->infile_fd = open(argv[1], O_RDWR | O_CREAT, 0777);
 	if (pipex->infile_fd == -1)
-        ft_error(0, "infile opening error");
+        ft_error(0, "infile open");
 	pipex->outfile_fd = open(argv[argc - 1], O_RDWR | O_CREAT, 0777);
 	if (pipex->outfile_fd == -1)
-        ft_error(0, "outfile opening error");
+        ft_error(0, "outfile open");
 }
 
 void	ft_fork(t_pipex *pipex, char **argv, char *arg, char **env)
@@ -24,89 +24,23 @@ void	ft_fork(t_pipex *pipex, char **argv, char *arg, char **env)
 	t_cmd	cmd;
 
 	if (pipe(pipe_fd) == -1)
-		ft_error(EXIT_FAILURE, "Open Pipe Error");
+		ft_error(EXIT_FAILURE, "Open Pipe");
 	pid = fork();
 	if (pid == -1)
-		ft_error(1, "Open Fork Error");
+		ft_error(1, "Open Fork");
 	if (pid == 0)
 	{
 		ft_get_path(env, arg, &cmd);
-		close(pipe_fd[0]);
+		dup2(pipex->infile_fd, STDIN_FILENO);
 		dup2(pipe_fd[1], STDOUT_FILENO);
-		exec(&cmd, env);
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+		exec(argv, &cmd, env);
 	}
 	waitpid(pid, NULL, 0);
 	dup2(pipe_fd[0], STDIN_FILENO);
-	//close(pipe_fd[0]);
-	//close(pipe_fd[1]);
+	dup2(pipex->outfile_fd, STDOUT_FILENO);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
 	//close(pipex->outfile_fd);
 }
-
-
-
-
-
-
-
-
-
-// void ft_fork_cmd1(int *pipe_fd, t_cmd *cmd, char *arg)
-// {
-// 	int	pid;
-
-// 	pid = fork();
-// 	if (pid == -1)
-// 		ft_error(1, "Open Fork Error");
-// 	if (pid == 0)
-// 	{
-// 		int file_fd;
-
-
-// 		// test
-// 		//dup2(file_fd, STDOUT_FILENO);
-
-// 		dup2(file_fd, STDIN_FILENO);	
-// 		dup2(pipe_fd[1], STDOUT_FILENO);
-// 		close(file_fd);
-// 		close(pipe_fd[0]);
-// 		close(pipe_fd[1]);
-// 		//cmd->output = get_next_line(file_fd);
-// 		char *exec_arg[] = {cmd->cmd_path, cmd->cmd_param, arg, NULL};
-// 		if (execve(cmd->cmd_path, exec_arg, NULL) == -1)
-//             ft_error(0, "cmd execution error");
-// 	}
-// }
-
-
-
-
-
-
-
-// void ft_fork_cmd2(int *pipe_fd, t_cmd *cmd2, char **argv)
-// {
-// 	int	pid2;
-	
-// 	pid2 = fork();
-// 	if (pid2 == -1)
-// 		ft_error(1, "Open Fork Error");
-// 	if (pid2 == 0)
-// 	{
-// 		int file_fd;
-		
-// 		file_fd = open(argv[4], O_RDWR | O_CREAT, 0777);
-//     	if (file_fd == -1)
-//         	ft_error(0, "file opening error");
-
-// 		dup2(pipe_fd[0], STDIN_FILENO);
-// 		dup2(file_fd, STDOUT_FILENO);
-// 		close(file_fd);
-// 		close(pipe_fd[0]);
-// 		close(pipe_fd[1]);
-
-// 		char *exec_arg[] = {cmd2->cmd_path, cmd2->cmd_param, cmd2->output, NULL};
-// 		if (execve(cmd2->cmd_path, exec_arg, NULL) == -1)
-//             ft_error(0, "cmd2 execution error");
-// 		close(file_fd);
-// 	}
-// }
--- src/main.c
+++ src/main.c
@@ -12,15 +12,6 @@
 
 #include "../includes/pipex.h"
 
-//pipex file1 cmd1 cmd2 file2
-//int execve(const char *pathname, char *const argv[], char *const envp[]);;
-// fd[0] - read
-// fd[1] - write
-// dup2(file_fd, STDIN)
-//	dup2(pipe_fd, STDOUT)
-//dip2(file, 1)
-//pipe, 0
-
 int	main(int argc, char **argv, char **env) {
 
 	t_pipex	pipex;
@@ -28,15 +19,13 @@ int	main(int argc, char **argv, char **env) {
 
 	i = 2;
 	if (argc != 5)
-	 	ft_error(EXIT_FAILURE, "Wrong Number of arguments");
+	 	ft_error(EXIT_FAILURE, "arg nbr");
 	ft_open_files(argv, argc, &pipex);
-	dup2(pipex.infile_fd, STDIN_FILENO);
+
 	while (i < (argc - 2))
 		ft_fork(&pipex, argv, argv[i++], env);
-	dup2(pipex.outfile_fd, STDOUT_FILENO);
-
-	char *arg[] = {"/bin/cat", "-e", argv[argc - 1], NULL};
+	char *arg[] = {"/bin/cat", "-e", "outfile", NULL};
 	if (execve("/bin/cat", arg, env) == -1)
-        ft_error(0, "cmd execution error");
+        ft_error(0, "cmd exec2");
 	exit(0);
 }
